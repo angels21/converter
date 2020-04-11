6de75dd386495454bcd896c8d4b684f1ddbb5818
angels21 <solotinted@yahoo.com>
2020-04-11 14:55:10 +0100

    converter

--- converter.go
+++ converter.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //Converter struct for various units
-type Converter struct{ Centimeter, Feet, Minutes, Seconds, Kilograms, Pounds, Fahrenheit, Celsius float64 }
+type Converter struct{ Centimeter, Feet, Minutes, Seconds, Kilograms, Pounds, Degree, Radian, Fahrenheit, Celsius float64 }
 
 //CentimeterToFeet converter
 func (cvr Converter) CentimeterToFeet() float64 {
@@ -35,6 +38,16 @@ func (cvr Converter) KilogramsToPounds() float64 {
 	return cvr.Kilograms * 2.205
 }
 
+//DegreeToRadian converter
+func (cvr Converter) DegreeToRadian() float64 {
+	return (cvr.Degree * 2 * math.Pi) / (360)
+}
+
+//RadianToDegree converter
+func (cvr Converter) RadianToDegree() float64 {
+	return (cvr.Radian * 360) / (2 * math.Pi)
+}
+
 //FahrenheitToCelsius converter
 func (cvr Converter) FahrenheitToCelsius() float64 {
 	return ((cvr.Fahrenheit - 32) * (9 / 5))
@@ -46,13 +59,15 @@ func (cvr Converter) CelsiusToFahrenheit() float64 {
 }
 func main() {
 
-	cvr := Converter{152.4, 5, 8, 6, 4.54, 10, 67, 35}
+	cvr := Converter{152.4, 5, 8, 6, 4.54, 10, 360, 6.286, 67, 35}
 	fmt.Println(cvr.Feet, "feet is: ", cvr.CentimeterToFeet(), "centimeter")
 	fmt.Println(cvr.Centimeter, "centimeter is: ", cvr.FeetToCentimeter(), "feet")
 	fmt.Println(cvr.Minutes, "Minutes is: ", cvr.MinutesToSeconds(), "seconds")
 	fmt.Println(cvr.Seconds, "Seconds is: ", cvr.SecondsToMilliseconds(), "Milliseconds")
 	fmt.Println(cvr.Pounds, "Pounds is: ", cvr.PoundsToKilograms(), "Kilograms")
 	fmt.Println(cvr.Kilograms, "Kilograms is: ", cvr.KilogramsToPounds(), "Pounds")
+	fmt.Println(cvr.Degree, "Degrees is: ", cvr.DegreeToRadian(), "Radians")
+	fmt.Println(cvr.Radian, "Radians is: ", cvr.RadianToDegree(), "Degrees")
 	fmt.Println(cvr.Fahrenheit, "Fahrenheit is: ", cvr.FahrenheitToCelsius(), "Celsius")
 	fmt.Println(cvr.Celsius, "Celsius is: ", cvr.CelsiusToFahrenheit(), "Fahrenheit")
 
